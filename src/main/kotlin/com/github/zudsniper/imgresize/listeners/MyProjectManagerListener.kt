4bbf0044865a5c7c8d7f919073debd27ac455202
GitHub Action <action@github.com>
2022-08-09 17:18:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/zudsniper/imgresize/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/zudsniper/imgresize/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zudsniper.imgresize.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.zudsniper.imgresize.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
